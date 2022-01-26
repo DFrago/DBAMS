80509c20d8e9a3c1781e7b20fadb4fff27781078
Frago1707 <85218257+Frago1707@users.noreply.github.com>
2022-01-26 15:45:42 +0300

    Styled ErrorPage

--- src/pages/errorPage.js
+++ src/pages/errorPage.js
@@ -5,10 +5,11 @@ import sadge from "./images/sadge.png"
 const Error=()=>{
     return(
         <section>
+        <div className="Body"></div>
         <h1 className="errorMessage">GG. Page not found</h1>
         <div className="errorImage">
         <img src={sadge} alt="sadge"/>
-        <p className="errorText">Use the logout button to return to the login screen</p>
+        <p className="errorText">Use the Logout Button to return to the login screen</p>
         </div>
         </section>
     )
